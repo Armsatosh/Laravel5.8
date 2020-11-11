b8e6868ef55e5ece5580e98a29adae5aa07368ab
Gevorg Stepanyan <gevorg.stepanyan100@gmail.com>
2020-11-11 17:40:32 +0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['namespace'=>'Blog', 'prefix'=>'blog'],function (){
+    Route::resource('posts','PostController')->names('blog.posts');
+});
+//Route::resource('rest','RestTestController')->names('restTest');
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+//----------- ADMIN PART -----------//
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'   => 'admin/blog'
+];
+Route::group($groupData,function (){
+    //BlogCategory
+    $methods = ['index','edit','store','update', 'create'];
+    Route::resource('categories','CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+    //BlogPost
+    Route::resource('posts','PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
+//------- End Admin Part -------//
